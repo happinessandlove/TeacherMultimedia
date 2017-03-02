ef4c9d7051b1b4af538cf7182c1ca091ab5d6a33
happinessandlove <1565803020@qq.com>
2017-03-03 00:40:48 +0800

    add UseDeviceController

---
+++ Helper/MvcHelper.Management/Controllers/UseDeviceController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcHelper.Management.Controllers
+{
+    public class UseDeviceController : Controller
+    {
+        // GET: UseDevice
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: UseDevice/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UseDevice/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UseDevice/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UseDevice/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UseDevice/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UseDevice/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UseDevice/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
